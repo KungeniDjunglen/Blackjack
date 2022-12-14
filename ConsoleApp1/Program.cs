fac41c342d551f75a9f79a5ce7e6549a704a3a65
KungeniDjunglen <111962627+KungeniDjunglen@users.noreply.github.com>
2022-12-14 12:10:21 +0100

    Den skriver långasmt nu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 
 // Blackjack V1 
 
@@ -8,67 +9,93 @@ namespace Kortlek
     {
         public string WinChecker(int up, int cp)
             {
+            var p = new Program();
                 if (up > 21)  // De två nedanstående if kollar om de har förlorat
                 {
-                    Console.WriteLine("Du förlorade med " + cp + " poäng.");
+                    p.Write("Du förlorade med " + cp + " poäng.");
                     return "c";
                 }
                 else if (cp > 21)
                 {
-                    Console.WriteLine("Du vann med " + up + " poäng :)");
+                    p.Write("Du vann med " + up + " poäng :)");
                     return "u";
                 }
                 return " ";
             }
+
+
         public int kortRND(int times)
         {            
             Random rnd = new Random();
             int answear = 0;
             for (int x = 0; x < times; x++)
             {
-                answear += rnd.Next(1,14);
+                answear += rnd.Next(1,13);
             }
 
             return answear;
         }
+        public string Write(string text)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(7);
+            }
+            Console.Write("\n");
+            return " ";
+        }
+
+
         static void Main(string[] args)
         {
+            var p = new Program();
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Kortlek v1");
+            p.Write("Kortlek v1");
             bool gameon = true;
             bool vinnare = false;
             while (gameon == true)
             {
-
-                Console.WriteLine("Välj ett av alternativen:");
-                Console.WriteLine("1. Spela blackjack");
-                Console.WriteLine("2. Visa spelets senaste vinnare");
-                Console.WriteLine("3. Spelets regler");
-                Console.WriteLine("4. Avsluta programet");
-                int val = int.Parse(Console.ReadLine());
+                Console.Clear();
+                int val = 0;
+                try
+                {
+                    p.Write("Välj ett av alternativen:");
+                    p.Write("1. Spela blackjack");
+                    p.Write("2. Visa spelets senaste vinnare");
+                    p.Write("3. Spelets regler");
+                    p.Write("4. Avsluta programet");
+                    val = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    p.Write("Skriv in ett nummer!");
+                }
+                
                 switch (val) {
                     case 1:
-                        var p = new Program();  // All här nedan kör i början av spelet för att spelarna ska ta två kort
+                        Console.Clear();
+                        // All här nedan kör i början av spelet för att spelarna ska ta två kort
                         int up = p.kortRND(2);
                         int cp = p.kortRND(2);
-                        Console.WriteLine("Dina poäng är: " + up);
-                        Console.WriteLine("Datorns poäng är: " + cp);
+                        p.Write("Dina poäng är: " + up);
+                        p.Write("Datorns poäng är: " + cp);
 
                         if (p.WinChecker(up,cp) == "u") { vinnare = true; break; }
                         if (p.WinChecker(up,cp) == "c") { vinnare = false; break; }
 
-                        Console.WriteLine("Vill du ta ett till kort? (y/n)");
+                        p.Write("Vill du ta ett till kort? (y/n)");
                         string gval = Console.ReadLine().ToLower();
                         if (gval == "y")  // Kör så länge spelaren vill fortsätta ta upp kort.
                         {
                             while (gval == "y")
                             {
                                 int x = p.kortRND(1);
-                                Console.WriteLine("Ditt nya kort är: " + x);
+                                p.Write("Ditt nya kort är: " + x);
                                 up = up + x;
                                 cp = cp + p.kortRND(1);
-                                Console.WriteLine("Dina poäng är: " + up);
-                                Console.WriteLine("Datorns poäng är: " + cp);
+                                p.Write("Dina poäng är: " + up);
+                                p.Write("Datorns poäng är: " + cp);
 
                                 if (p.WinChecker(up, cp) == "u") { vinnare = true; break; }
                                 if (p.WinChecker(up, cp) == "c") { vinnare = false; break; }
@@ -87,44 +114,54 @@ namespace Kortlek
                         }
                         else
                         { // om man skriver inn något dummt istället för y/n
-                            Console.WriteLine("y/n vad är det du inte fattar?");
+                            p.Write("y/n vad är det du inte fattar?");
                             break;
                         }
 
                         if (up > cp && up < 22)    //Slutchecken för att kolla vem som vann
                         {
-                            Console.WriteLine("Du vann med: " + up + " poäng! :)");
+                            p.Write("Du vann med: " + up + " poäng! :)");
                             vinnare = true;
                         }
                         else if (cp > up && cp < 22)
                         {
-                            Console.WriteLine("Datorn vann med: " + cp + " poäng! :(");
+                            p.Write("Datorn vann med: " + cp + " poäng! :(");
                             vinnare = false;
                         }
+                        Console.ReadKey();
                         break;
                     case 2:
+                        Console.Clear();
                         if (vinnare == false)
                         {
-                            Console.WriteLine("Datorn vann senast");
+                            p.Write("Datorn vann senast");
                         }
                         if (vinnare == true)
                         {
-                            Console.WriteLine("Spelaren vann senast");
+                            p.Write("Spelaren vann senast");
                         }
+                        Console.ReadKey();
                         break;
                     case 3:
-                        Console.WriteLine("Målet med blackjack är att komma så när 21 som möjligt utan att passera det.");
+                        Console.Clear();
+                        p.Write("    Målet med blackjack är att komma så när 21 som möjligt utan att passera det. \n    " +
+                            "När spelet börjar tar både du och datorn två kort, kortens poäng varierar mellan 1-13. \n    Får man över 21 poäng förlorar du, den som har mest" +
+                            " poäng i slutet vinner. \n");
+                        
+                        Console.ReadKey();
                         break;
                     case 4:
                         gameon = false;
+
+                        break;
+                    default:
+                        p.Write("??");
+                        Console.ReadKey();
                         break;
                 }
-                Console.WriteLine("\n \n \n \n \n");
 
             }
-            Console.WriteLine("Tack för att du har spelat spelet");
-            
-
+            p.Write("Tack för att du har spelat spelet");
         }
     }
 }
